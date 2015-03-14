22dd9c4f806cb2131d40f624dd6d7b407c69ed9a
Randall Rodakowski <randall.rodakowski@gmail.com>
2015-03-14 14:18:36 -0400

    cleanup, removing some files and moved the useful pieces into services

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# python-scripts
-python scripts (and sample code) for tiny jobs
+### python-services
+The main file in this repository is services.py. This is a collection of python classes that I find useful when writing python scripts for small tasks. The rest of the files are python scripts (and sample code) that I still for reference. These will be removed over time as they are folded into the services module.
 
-This repository is a personal collection of python snippets that I find useful and refer to for small daily tasks. 
--- services.py
+++ services.py
@@ -1,7 +1,10 @@
-import time
+import urlparse, string, csv, os, time, logging
 from sys import argv
 from subprocess import call
-import logging
+from urllib2 import urlopen
+from socket import socket
+
+
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +13,22 @@ class FileService:
 
     def __init__(self):
         logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(module)s %(message)s')
+
+    def get_first_and_last_column(filename, separator):
+        with file(filename, 'rb') as file_obj:
+            for line in csv.reader(file_obj,
+                delimiter=separator,    # Your custom delimiter.
+                skipinitialspace=True): # Strips whitespace after delimiter.
+                if line: # Make sure there's at least one entry.
+                    yield line[0], line[-1]
+
+    #useful for building novus load files
+    def jar_files_in_dir(self, directory):
+        for f in os.listdir(directory):
+            if f.endswith(".gz"):
+                no_ext=f[:-3]
+                new_filename=no_ext+".jar"
+                call('jar cvfM {} {}'.format(new_filename, f), shell=True)
 
     def write_to_file(self,filename, text):
         logger.info("Writing the file: "+filename)
@@ -48,3 +67,64 @@ class FileService:
         subject = 'subject: {} {} {}'.format(time.asctime(), msg_subject.upper(), server_info)
         email=self.create_email_file(subject, msg_body)
         call('sendmail -v {} < {}'.format(email_address, email), shell=True)
+
+
+class ServiceMonitor(object):
+
+    def usage(self):
+        print('%s <test-type> <server-info> <email-address>\n' % (argv[0]))
+        print('\ttest-type    \ttcp or http')
+        print('\tserver-info  \thostname:port for tcp')
+        print('\t             \thttp://hostname/page for http')
+        print('\temail-address\tusername or username@domain.com\n')
+
+    def tcp_test(self, server_info):
+        cpos = server_info.find(':')
+        if cpos < 1 or cpos == len(server_info) - 1:
+            print('You need to give server info as hostname:port.')
+        self.usage()
+        return True
+        try:
+            sock = socket()
+            sock.connect((server_info[:cpos], int(server_info[cpos+1:])))
+            sock.close
+            return True
+        except:
+            return False
+
+    def http_test(self, server_info):
+        try:
+            data = urlopen(server_info).read()
+            return True
+        except:
+            print('Could not read from the server')
+            return False
+
+    def server_test(self, test_type, server_info):
+        if test_type.lower() == 'tcp':
+            return self.tcp_test(server_info)
+        elif test_type.lower() == 'http':
+            return self.http_test(server_info)
+        else:
+            print('Invalid test-type given, please use either tcp or http.')
+            return True
+
+    def send_error(self, test_type, server_info, email_address):
+        subject = '%s: %s %s error' % (time.asctime(), test_type.upper(), server_info)
+        message = 'There was an error while executing a %s test against %s.' % (test_type.upper(), server_info)
+        os.system('echo "%s" | mail -s "%s" %s' % (message, subject, email_address))
+
+class ScreenScraper(object):
+
+    def get_temperature(self, country, state, city):
+        url = urlparse.urljoin('http://www.weather.com/weather/cities/',
+                           string.lower(country)+'_' + \
+                           string.lower(state) + '_' + \
+                           string.replace(string.lower(city), ' ',
+                                          '_') + '.html')
+        data = urlopen(url).read()
+        start = string.index(data, 'current temp: ') + len('current temp: ')
+        stop = string.index(data, '&degv;F', start-1)
+        temp = int(data[start:stop])
+        localtime = time.asctime(time.localtime(time.time()))
+        print('On {}, the temperature in {}, {}, {} is {}.'.format(localtime,city, state, country, temp))
